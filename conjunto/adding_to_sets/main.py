d9ea84afeb1a0722141b2f02134b151e1fcedb18
velezjosemedinaq-pixel <>
2026-03-29 03:25:10 +0000

    Adding to Sets

---
+++ conjunto/adding_to_sets/main.py
@@ -0,0 +1,15 @@
+tarantino_movies = {
+    'Pulp Fiction',
+    'Kill Bill: Vol. 1',
+    'Kill Bill: Vol. 2',
+    'Inglourious Basterds',
+    'Django Unchained'
+}
+
+# Write your code here
+# Añade las dos películas al set correcto
+tarantino_movies.add('Reservoir Dogs')
+tarantino_movies.add('Once Upon a Time in Hollywood')
+
+# Testing
+print("Updated set:", tarantino_movies)
